7adde0f59185778d90eab1f7eba3331c50d8268d
RuiS1999 <rui.2805.work@gmail.com>
2023-03-25 19:19:30 +0900

    echoで出力

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8" />
+    </head>
+    <title>文字列の出力</title>
+    <body>
+        <?php
+            echo 'Hello World';
+        ?>
+        <br>
+        <?php
+            echo 4*7;
+        ?>
+        <br>
+        <?php
+            echo 8%3;
+        ?>
+    </body>
+</html>
