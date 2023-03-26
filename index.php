e80b90df206cced17d1bfef84c56a9e05a5b73f1
RuiS1999 <rui.2805.work@gmail.com>
2023-03-26 18:32:33 +0900

    条件否定文

--- index.php
+++ index.php
@@ -8,13 +8,11 @@
     <title>文字列の出力</title>
     <body>
         <?php
-            $sum = 1*10;
-            if ($sum <= 10 ) {
-                echo '10以下';
-            } elseif ($sum >10 && $sum <= 99) {
-                echo '11以上99以下';
-            } else {
-                echo '100以上';
+            //条件否定文を用いる際は
+            // if (!(条件分)) {}
+            $sum = 1*9;
+            if (!($sum > 10 )) {
+                echo '10未満';
             }
         ?>
         <br>
